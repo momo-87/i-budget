117099cbd6946c27a229a76391d28c156003644b
Momo-87 <momochristian87@gmail.com>
2023-09-14 10:56:34 +0100

    Update model unit tests to consider Devise specifications

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: 'Christian Momo') }
+  let(:user) { User.create!(name: 'Christian Momo', email: 'mm@recipapp.com', password: '123456') }
 
   describe 'validation' do
     it 'requires name to be present' do
